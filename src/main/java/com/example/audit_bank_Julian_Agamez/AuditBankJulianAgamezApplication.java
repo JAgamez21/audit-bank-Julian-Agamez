4aeebc3484b501d305f27c377d2286af86f7dfc0
JAgamez21 <zjulian1208@gmail.com>
2025-01-15 23:03:44 -0500

    feat: initial setup of Audit-Bank project

---
+++ src/main/java/com/example/audit_bank_Julian_Agamez/AuditBankJulianAgamezApplication.java
@@ -0,0 +1,13 @@
+package com.example.audit_bank_Julian_Agamez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditBankJulianAgamezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditBankJulianAgamezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/audit_bank_Julian_Agamez/AuditBankJulianAgamezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.audit_bank_Julian_Agamez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditBankJulianAgamezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
